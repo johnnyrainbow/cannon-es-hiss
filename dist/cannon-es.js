1843cb5660e6c23b1623956f410af14bc8664b7e
Gabriel <gabriel.m.kennedy@gmail.com>
2024-05-29 09:04:13 +0200

    h

--- dist/cannon-es.js
+++ dist/cannon-es.js
@@ -9049,18 +9049,18 @@ class GSSolver extends Solver {
         //therefore, we must apply the mutation to the customIndependentVelocity also.
         //so that collisions will work.
         const v = b.velocity;
-        const v2 = b.customIndependentVelocity;
+        b.customIndependentVelocity;
         const w = b.angularVelocity;
         b.vlambda.vmul(b.linearFactor, b.vlambda);
         v.vadd(b.vlambda, v);
-        const posv2x = Math.abs(b.vlambda.x);
-        const posv2z = Math.abs(b.vlambda.z);
+        Math.abs(b.vlambda.x);
+        Math.abs(b.vlambda.z);
         // v2.vadd(b.vlambda, v2)
 
-        if (posv2x > 300 || posv2z > 300) {
-          v2.vadd(b.vlambda, v2);
-          console.log('DID YOU KNOW YOUR vlambdaoo5?', b.vlambda);
-        }
+        // if (posv2x > 300 || posv2z > 300) {
+        //   v2.vadd(b.vlambda, v2)
+        //   console.log('DID YOU KNOW YOUR vlambdaoo5?', b.vlambda)
+        // }
         // if (Math.abs(v2.x) > 30 || Math.abs(v2.z) > 30) {
         //   // console.log("SET CUSTOM VELOOCH ", v2)
         //   v2.vadd(b.vlambda, v2)
--- src/solver/GSSolver.ts
+++ src/solver/GSSolver.ts
@@ -135,10 +135,10 @@ export class GSSolver extends Solver {
         const posv2z = Math.abs(b.vlambda.z)
         // v2.vadd(b.vlambda, v2)
 
-        if (posv2x > 300 || posv2z > 300) {
-          v2.vadd(b.vlambda, v2)
-          console.log('DID YOU KNOW YOUR vlambdaoo5?', b.vlambda)
-        }
+        // if (posv2x > 300 || posv2z > 300) {
+        //   v2.vadd(b.vlambda, v2)
+        //   console.log('DID YOU KNOW YOUR vlambdaoo5?', b.vlambda)
+        // }
         // if (Math.abs(v2.x) > 30 || Math.abs(v2.z) > 30) {
         //   // console.log("SET CUSTOM VELOOCH ", v2)
         //   v2.vadd(b.vlambda, v2)
