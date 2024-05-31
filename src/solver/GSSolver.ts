696519bb9507e98682b083377bb685299d15523b
Gabriel <gabriel.m.kennedy@gmail.com>
2024-05-31 18:38:25 +0200

    e

--- src/solver/GSSolver.ts
+++ src/solver/GSSolver.ts
@@ -107,7 +107,7 @@ export class GSSolver extends Solver {
 
           deltalambdaTot += deltalambda > 0.0 ? deltalambda : -deltalambda // abs(deltalambda)
           if (deltalambda > 50) {
-            // console.log(invC, B, GWlambda, c.eps, lambdaj)
+            console.log(invC, B, GWlambda, c.eps, lambdaj)
           }
           c.addToWlambda(deltalambda)
         }
